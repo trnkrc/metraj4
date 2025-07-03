a888a31f60d25ff4582bc9b983dad91ad7c50494
trnkrc <turankirac@gmail.com>
2025-07-03 16:12:47 +0300

    Delete metraj-app directory

--- metraj-app/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import MetrajApp from "./MetrajApp";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <MetrajApp />
-  </React.StrictMode>
-);
